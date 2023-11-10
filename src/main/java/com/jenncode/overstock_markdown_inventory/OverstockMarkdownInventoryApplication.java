7569a8249d9c4ca1890fca99980f4a54919a8bf4
Jenn <jenniferjuarezjj@gmail.com>
2023-11-10 13:48:24 -0500

    initial commit

---
+++ src/main/java/com/jenncode/overstock_markdown_inventory/OverstockMarkdownInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.jenncode.overstock_markdown_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverstockMarkdownInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverstockMarkdownInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenncode/overstock_markdown_inventory/OverstockMarkdownInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenncode.overstock_markdown_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverstockMarkdownInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
